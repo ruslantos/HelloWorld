f41cf9276df0c34c8ca67573e4316bf63dbced18
ruslantos <ruslantos@mail.ru>
2019-01-12 18:55:50 +0300

    Initial commit

---
+++ src/Arrays/ExEquals.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+import java.util.Arrays;
+
+
+public class ExEquals {
+    public static void main(String[] args) {
+        int [] a = {1,2,3,5,6};
+        int [] b = {1,2,3,5,6};
+        System.out.println(Arrays.equals(a,b));
+    }
+}
